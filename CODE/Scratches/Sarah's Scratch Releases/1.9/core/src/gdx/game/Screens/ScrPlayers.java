2799aee75f958c0d3fda1c9b3ce08109e95ddcd3
Sarah Nguyen <itssarahnguyen@hotmail.com>
2018-06-16 15:31:56 -0400

    Release 1.8 & 1.9 - not official

---
+++ CODE/Scratches/Sarah's Scratch Releases/1.9/core/src/gdx/game/Screens/ScrPlayers.java
@@ -0,0 +1,116 @@
+package gdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import gdx.game.Menu.GamMenu;
+import gdx.game.Objects.Title;
+
+public class ScrPlayers implements Screen, InputProcessor {
+    Texture txBg;
+    Sprite sprBg;
+    Title ttlStart;
+    SpriteBatch batch;
+    GamMenu gamMenu;
+    OrthographicCamera oc;
+
+    public ScrPlayers(GamMenu _gamMenu) {
+        gamMenu = _gamMenu;
+    }
+
+    @Override
+    public void show() {
+        //Texture
+        txBg = new Texture("playerselbckgd.jpg");
+        //Sprite
+        batch = new SpriteBatch();
+        sprBg = new Sprite(txBg);
+        sprBg.setSize(800, 500);
+        sprBg.setFlip(false, true);
+        //ortho
+        oc = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        oc.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        oc.update();
+        Gdx.input.setInputProcessor(this);
+        //Title
+        ttlStart = new Title(400, 400, Gdx.graphics.getWidth() / 2 - 188, Gdx.graphics.getHeight() / 2 - 350, "title.png");
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.setProjectionMatrix(oc.combined);
+        batch.begin();
+        sprBg.draw(batch);
+        ttlStart.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
