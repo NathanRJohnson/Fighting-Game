b441a77871e557a56e89dbc45530a2aee03e4ecf
Nathan Johnson <robert.minecrafter.nathan@gmail.com>
2018-06-20 10:34:03 -0400

    Files that need to be remade

---
+++ CODE/Releases/2.2/core/src/gdx/game/Objects/SprFighter.java
@@ -0,0 +1,93 @@
+package gdx.game.Objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+public class SprFighter extends Sprite {
+    int nPlayer;
+    public Vector2 v2Location, v2Velocity, v2Acceleration;
+   public boolean canMove;
+    double dMaxFall;
+    double dCeiling = 230; //Maximum Jump Height
+    double dDelay;
+    public boolean isBlocking;
+    public int BasicDamage = 10;
+
+    public SprFighter(String sFile, int nX, int nY, int nW, int nH, int _nPlayer) {
+        super(new Texture(sFile));
+        v2Location = new Vector2(nX, nY);
+        v2Velocity = new Vector2(0, 0);
+        v2Acceleration = new Vector2(0, 0);
+        canMove = true;
+        dMaxFall = 5;
+        setSize(nW, nH);
+        setPosition(nX, nY);
+        nPlayer = _nPlayer;
+        if (nPlayer == 1) {
+            setFlip(false, false);
+        } else {
+            setFlip(true, false);
+        }
+    }
+
+    public void move(double dTime) {
+        v2Velocity.add(v2Acceleration);
+        v2Location.add(v2Velocity);
+        v2Acceleration.setZero(); //idk
+
+        if (nPlayer == 1) {
+            dDelay += 0.1;
+            if (Gdx.input.isKeyPressed(Input.Keys.A) && dTime >= 2 && canMove == true)
+                v2Location.x -= 4f;
+
+            if (Gdx.input.isKeyPressed(Input.Keys.D) && dTime >= 2 && canMove == true)
+                v2Location.x += 4f;
+
+            if (v2Location.y < dCeiling && dDelay > 3) {
+                if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+                    dDelay = 0;
+                    v2Velocity.y = 10f;
+                }
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+                isBlocking = true;
+                //System.out.println("Am blocking");
+            } else {
+                isBlocking = false;
+                // System.out.println("Not Blocking");
+            }
+        }
+
+        if (nPlayer == 2) {
+            dDelay += 0.1;
+            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)&& dTime >= 2 && canMove)
+                v2Location.x -= 4f;
+
+            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)&& dTime >= 2 && canMove)
+                v2Location.x += 4f;
+
+            if (v2Location.y < dCeiling && dDelay > 3) {
+                if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                    v2Velocity.y = 10f;
+                    dDelay = 0;
+                }
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                isBlocking = true;
+                //System.out.println("Am blocking");
+            } else {
+                isBlocking = false;
+                // System.out.println("Not Blocking");
+            }
+        }
+    }
+
+    public void applyForce(Vector2 v2Force) {
+        Vector2 f = v2Force.cpy();
+        v2Acceleration.add(f);
+
+    }
+}
